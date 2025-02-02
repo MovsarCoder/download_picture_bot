a50e71c7219a01b3e6e01c26339c9b2e20cf7bc6
DonviteCorleone <blackstar013.bs@gmail.com>
2025-02-03 02:21:37 +0300

    Обновил названия фунций

--- handlers/admin_panel/admin_handler.py
+++ handlers/admin_panel/admin_handler.py
@@ -13,7 +13,7 @@ router = Router()
 @router.message(Command('admin_panel'))
 @router.callback_query(F.data == 'admin_data')
 async def cmd_admin(callback_or_message: CallbackQuery | Message):
-    admin_users_list = checked_admin_list()
+    admin_users_list = get_admin_list()
     if callback_or_message.from_user.id in admin_users_list:
         if isinstance(callback_or_message, CallbackQuery):
             await callback_or_message.message.edit_text('Выберите действие', reply_markup=make_row_inline_keyboards(add_new_admin_user_keyboard))
@@ -27,7 +27,7 @@ async def cmd_admin(callback_or_message: CallbackQuery | Message):
 
 @router.callback_query(F.data == 'new_admin_data')
 async def new_admin_user_func(callback: CallbackQuery, state: FSMContext):
-    await callback.message.edit_text('Введите ID пользователя, которого хотите добавить как админ', reply_markup=make_row_inline_keyboards(back_keyboard))
+    await callback.message.answer('Введите ID пользователя, которого хотите добавить как админ', reply_markup=make_row_inline_keyboards(back_keyboard))
     await state.set_state(AdminState.new_admin)
 
 
@@ -35,15 +35,13 @@ async def new_admin_user_func(callback: CallbackQuery, state: FSMContext):
 async def add_admin_id(message: Message, state: FSMContext):
     await state.update_data(id=message.text)
     try:
-        a = add_new_admin_db(message.text)
+        add_admin(message.text)
         # Добавляем ID пользователя в список администраторов
-        if a:
-            print('Администратор был успешно добавлен в базу данных')
-            await message.answer(f'Пользователь с ID {message.text} добавлен как админ.', reply_markup=make_row_inline_keyboards(add_new_admin_user_keyboard))
-            await state.clear()
+
+        print('Администратор был успешно добавлен в базу данных')
+        await message.answer(f'Пользователь с ID {message.text} добавлен как админ.', reply_markup=make_row_inline_keyboards(add_new_admin_user_keyboard))
+        await state.clear()
         # Не удалось найти пользователя по данному ID
-        else:
-            await message.answer('Не удалось найти пользователя по этому ID или уже существует такой Администратор!', reply_markup=make_row_inline_keyboards(add_new_admin_user_keyboard))
     except ValueError as e:
         await message.answer(f'Ошибка добавления пользователя. Ошибка: {e}', reply_markup=make_row_inline_keyboards(add_new_admin_user_keyboard))
 
@@ -51,21 +49,17 @@ async def add_admin_id(message: Message, state: FSMContext):
 @router.callback_query(F.data == 'remove_admin_list_data')
 async def remove_admin_func(callback: CallbackQuery, state: FSMContext):
     await callback.answer('')
-    await callback.message.edit_text('Введите ID пользователь, которого хотите удалить', reply_markup=make_row_inline_keyboards(back_keyboard))
+    await callback.message.answer('Введите ID пользователь, которого хотите удалить', reply_markup=make_row_inline_keyboards(back_keyboard))
     await state.set_state(AdminState.remove_admin)
 
 
 @router.message(AdminState.remove_admin)
-async def remove_admin(message: Message, state: FSMContext):
+async def remove_admin_func(message: Message, state: FSMContext):
     try:
-        if remove_admin_from_db(message.text):
-            # Администратор успешно удален из базы данных.
-            await message.answer(f'Пользователь с ID {message.text} был удален!', reply_markup=make_row_inline_keyboards(add_new_admin_user_keyboard))
-            await state.clear()
-        else:
-            # Не удалось найти администратора в базе данных.
-            await message.answer('Не удалось найти администратора в базе данных.', reply_markup=make_row_inline_keyboards(add_new_admin_user_keyboard))
-
+        remove_admin(message.text)
+        # Администратор успешно удален из базы данных.
+        await message.answer(f'Пользователь с ID {message.text} был удален!', reply_markup=make_row_inline_keyboards(add_new_admin_user_keyboard))
+        await state.clear()
     except ValueError as e:
         await message.answer(f'Ошибка удаления пользователя. Ошибка: {e}', reply_markup=make_row_inline_keyboards(add_new_admin_user_keyboard))
 
@@ -73,8 +67,8 @@ async def remove_admin(message: Message, state: FSMContext):
 @router.callback_query(F.data == 'add_new_group_username_data')
 async def add_new_group_username_db(callback: CallbackQuery, state: FSMContext):
     await callback.answer('')
-    await callback.message.edit_text('Отправьте username канала/группы (без использования @)!',
-                                     reply_markup=make_row_inline_keyboards(back_keyboard))
+    await callback.message.answer('Отправьте username канала/группы (без использования @)!',
+                                  reply_markup=make_row_inline_keyboards(back_keyboard))
     await state.set_state(AdminState.add_new_group_username)
 
 
@@ -97,7 +91,7 @@ async def fsm_add_new_group_name(message: Message, state: FSMContext):
     }
 
     # Если такая группа с таким Username присутствует, выводится данное сообщение.
-    if not writer_group_to_json(group_data):
+    if not add_group(group_data):
         await message.answer(f'Ошибка! Группа с таким username уже существует: {group_data["username"]}', reply_markup=make_row_inline_keyboards(add_new_admin_user_keyboard))
         return False
 
@@ -120,7 +114,7 @@ async def fsm_remove_group_db(message: Message, state: FSMContext):
         # Переменная для ловли сообщения от пользователя
         message_text = message.text
         # Если с написанным пользователем Username присутствует, то она удалится.
-        remove_func = remove_group_from_json(message_text)
+        remove_func = remove_group(message_text)
         # Если функция remove_func возвращает True - группа удаляется и выводится сообщение
         if remove_func:
             await message.answer(f'Группа с Username: {message_text} успешно удалена!', reply_markup=make_row_inline_keyboards(add_new_admin_user_keyboard))
@@ -136,12 +130,12 @@ async def fsm_remove_group_db(message: Message, state: FSMContext):
 @router.callback_query(F.data == 'list_group_data')
 async def group_list_db(callback: CallbackQuery):
     await callback.answer('')
-    groups = load_from_json()
+    groups = load_groups()
     keyboard = []
 
     # если в JSON-файле нет никаких групп для подписки, выведется данное сообщение
     if not groups:
-        await callback.message.edit_text("Нет добавленных групп.", reply_markup=make_row_inline_keyboards(add_new_admin_user_keyboard))
+        await callback.message.answer("Нет добавленных групп.", reply_markup=make_row_inline_keyboards(add_new_admin_user_keyboard))
         return
     # если в JSON-файле есть группа из нее создастся клавиатура
     for group in groups:
