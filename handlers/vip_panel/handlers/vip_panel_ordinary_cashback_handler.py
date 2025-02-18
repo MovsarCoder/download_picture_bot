4f7e43b33d4e55730ddaef9962ccd4e01ca2742e
DonviteCorleone <blackstar013.bs@gmail.com>
2025-02-19 00:04:06 +0300

    Исправлен текст отправляемый после нажатия на кнопку парсинга кэшбека. Теперь текст более солидный и с приведенным примером, что да как нужно отправить чтобы получился идеальный парсер.
    Также была изменена чуть-чуть архитектура. Все похожие и одинаковые вещи были перемещены ближе друг к другу.
    
    Также добавлен новый ParseMode для того, чтобы пример отправки запроса на парсинг был более удобный для чтения глаз.

--- handlers/vip_panel/handlers/vip_panel_ordinary_cashback_handler.py
+++ handlers/vip_panel/handlers/vip_panel_ordinary_cashback_handler.py
@@ -1,8 +1,10 @@
 import os
 import time
 from aiogram import F, Router
+from aiogram.enums import ParseMode
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery, BufferedInputFile
+
 from States.state import WildberriesCashback
 from config.config import ADMIN
 from keyboard.keyboard import more_xlsx_product_keyboard, show_vip_keyboard
@@ -17,7 +19,9 @@ async def send_name_product_func(callback: CallbackQuery, state: FSMContext):
     await state.clear()
     await callback.answer('')
 
-    await callback.message.answer('Введите название товара для поиска <b>Кешбэка</b>: ')
+    await callback.message.answer("Запущен парсер кэшбека Wildberries по поисковому запросу или категории! Вставьте ссылку на категорию или напишите запрос. Например: ```https://www.wildberries.ru/catalog/dlya-doma/predmety-interera/svechi-i-podsvechniki``` Или ```Платье женское``` ",
+                                  parse_mode=ParseMode.MARKDOWN)
+
     await state.set_state(WildberriesCashback.get_name_product)
 
 
@@ -25,17 +29,19 @@ async def send_name_product_func(callback: CallbackQuery, state: FSMContext):
 async def get_name_product_func(message: Message, state: FSMContext):
     await state.update_data(name_product=message.text)
     data = await state.get_data()
-    name_product = data['name_product']
 
-    send_name_box = await message.answer(f'Файл по запросу: <b>{name_product}</b> собирается. Ожидайте!')
+    await message.answer(f'⚙️ Парсер начал работу...')
+
+    # Запуск парсера
+    name_product = data['name_product'] # Получаем введеный запрос пользователя
+    await main(name_product)
 
     # Получаем администратора для бота
     admin = ADMIN
-
     start_time = time.time()
-    await main(name_product)
     end_time = time.time()
     show_time = int(end_time - start_time)
+
     # Объединяем главную клавиатуру Vip Panel с клавиатурой на которой кнопка "Другой запрос"
     keyboard = show_vip_keyboard + more_xlsx_product_keyboard
 
@@ -49,12 +55,11 @@ async def get_name_product_func(message: Message, state: FSMContext):
                                                     f'<b>Запрос:</b> "<i>{name_product}</i>"\n\n'
                                                     f'Вот ваш файл с данными.',
                                             reply_markup=make_row_inline_keyboards(keyboard))
-        await send_name_box.delete()
+
         os.remove(f'../this_bot/{name_product}.csv')
         await state.clear()
 
     except Exception as ex:
-        await send_name_box.delete()
         await message.answer(f'Запрос не обработан по причине: {ex}')
         os.remove(f'../this_bot/{name_product}.csv')
 
