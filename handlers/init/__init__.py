69d90c11f16c5c5e8500797944da159a26d89680
DonviteCorleone <blackstar013.bs@gmail.com>
2025-02-26 01:19:04 +0300

    Добавлен новый роутер на покупку вип панели.

--- handlers/init/__init__.py
+++ handlers/init/__init__.py
@@ -18,11 +18,12 @@ router.include_router(video_handler)
 from handlers.admin.handlers.admin_handler import router as admin_handler
 router.include_router(admin_handler)
 
-
+from handlers.vip.handlers.buy_vip_panel import router as buy_vip_panel
 from handlers.vip.handlers.vip_panel_handler import router as vip_panel_router
 from handlers.vip.handlers.vip_panel_ordinary_cashback_handler import router as ordinary_cashback_router
 from handlers.vip.handlers.vip_panel_super_cashback_handler import router as super_cashback_router
 from handlers.vip.handlers.vip_panel_pars_all_product_handler import router as pars_all_product_router
+router.include_router(buy_vip_panel)
 router.include_router(vip_panel_router)
 router.include_router(ordinary_cashback_router)
 router.include_router(super_cashback_router)
