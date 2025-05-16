03a3f677bda0020756a51917ceedc54ff3aedae5
DonviteCorleone <blackstar013.bs@gmail.com>
2025-05-16 23:46:41 +0300

    Дописание моделей. Дописал модели Groups/Admin/Vip. С использованием __repr__

--- database/models_sqlalchemy.py
+++ database/models_sqlalchemy.py
@@ -1,47 +1,56 @@
-from enum import unique
-
-from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime
 from datetime import datetime
-from database.database_sqlalchemy import Base
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
 
 
 class User(Base):
-    """
-    """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    username = Column(String(120))
-    fullname = Column(String(120))
-    firstname = Column(String(120))
-    lastname = Column(String(120))
-    telegram_id = Column(Integer, unique=True)
-    registration_date = Column(DateTime, default=datetime.utcnow)
+    username = Column(String(120), nullable=True)  # username может быть не у всех
+    fullname = Column(String(120), nullable=False)  # обязательное поле
+    firstname = Column(String(120), nullable=True)
+    lastname = Column(String(120), nullable=True)
+    telegram_id = Column(Integer, unique=True, nullable=False)
+    registration_date = Column(DateTime, default=datetime.utcnow, nullable=False)
+
+
+
+    def __repr__(self):
+        return f"{self.id}, {self.username}, {self.fullname}, {self.firstname}, {self.lastname}, {self.telegram_id}, {self.registration_date}"
 
 
 class Groups(Base):
-    """
-    """
     __tablename__ = 'groups_list'
 
     id = Column(Integer, primary_key=True)
-    username = Column(String(120))
-    name = Column(String(120))
+    username = Column(String(120), unique=True, nullable=False)
+    name = Column(String(120), nullable=False)
+
+
+    def __repr__(self):
+        return f"id={self.id}, name='{self.name}')"
 
 
 class Admins(Base):
-    """
-    """
     __tablename__ = 'admin_list'
 
     id = Column(Integer, primary_key=True)
-    telegram_id = Column(Integer, unique=True)
+    telegram_id = Column(Integer, unique=True, nullable=False)
+
+
+    def __repr__(self):
+        return f"id={self.id}, telegram_id={self.telegram_id})"
 
 
 class Vip(Base):
-    """
-    """
     __tablename__ = 'vip_panel'
+
     id = Column(Integer, primary_key=True)
-    telegram_id = Column(Integer, unique=True)
-    name = Column(String(120))
+    telegram_id = Column(Integer, unique=True, nullable=False)
+    name = Column(String(120), nullable=True)
+
+    def __repr__(self):
+        return f"<Vip(id={self.id}, name='{self.name}')>"
