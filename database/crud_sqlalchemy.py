20defce7cf88ca24f4d170d1a4f7949c128be031
DonviteCorleone <blackstar013.bs@gmail.com>
2025-06-01 16:59:20 +0300

    Добавлена функция проверяющая находится ли пользователь с таким telegram_id в базе или нет. Вернет True - если есть. False - если нет

--- database/crud_sqlalchemy.py
+++ database/crud_sqlalchemy.py
@@ -1,12 +1,12 @@
 import asyncio
 import logging
-from typing import Optional
+from typing import Optional, Union
 
 from sqlalchemy import select, exists
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 
 from database.database_sqlalchemy import AsyncSessionLocal
-from database.models_sqlalchemy import User, Admins
+from database.models_sqlalchemy import User, Admins, Groups
 
 
 async def write_user(username: str,
@@ -36,12 +36,36 @@ async def write_user(username: str,
             session.add(user)
             await session.commit()
             await session.refresh(user)
-            logging.info(f'Пользователь добавлен! {user}')
+            logging.info(f'Пользователь добавлен! {user=}')
             return user
 
         except SQLAlchemyError:
             await session.rollback()
-            logging.warning(f"Пользователь с таким telegram_id={telegram_id} уже существует!")
+            logging.warning(f"Пользователь с таким {telegram_id=} уже существует!")
+
+
+async def user_exists(telegram_id: int, async_session_factory: AsyncSessionLocal = AsyncSessionLocal) -> bool:
+    """
+    Функция на просмотр, есть ли пользователь с таким ID или нет.
+    :param telegram_id: Ожидается telegram_id пользователя, по которому будем идти проверка, существует такой user или нет.
+    :param async_session_factory: Асинхронная Фабрика сессий
+    :return:
+
+    Examples:
+        print(await user_exists(567898765))
+
+    """
+
+    async with async_session_factory() as session:
+        stmt = select(exists().where(User.telegram_id == telegram_id))
+        result = await session.execute(stmt)
+        return result.scalar()
+
+
+async def main():
+    pass
 
 
 
+
+asyncio.run(main())
