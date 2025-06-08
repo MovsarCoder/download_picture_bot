3ea1980fddddd8d035aa77f64e9b13d6f33a9b77
DonviteCorleone <blackstar013.bs@gmail.com>
2025-06-09 01:46:16 +0300

    Добавлена новая функция на рассылку любого типа документа всем пользователям из базы данных!

--- handlers/admin/handlers/newsletter_handler.py
+++ handlers/admin/handlers/newsletter_handler.py
@@ -1,71 +1,47 @@
 from aiogram import F, Router
 from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
+
 from States.state import *
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from database.crud import get_chat_id
 
 router = Router()
 
-Base = declarative_base()
 
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
-
-
-# Инициализация базы данных
-DATABASE_URL = "sqlite:///bot.db"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-# Модель пользователя
-class User(Base):
-    __tablename__ = "users"  # Исправлено на __tablename__
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    chat_id = Column(String, unique=True, nullable=False)
-
-
-# Обработчик текстовых и медиа-сообщений в состоянии Letter.text
+# Обработчик текстовых и медиа-сообщений в состоянии NewsLetter.text
 @router.message(NewsLetter.text, F.content_type.in_({"text", "photo", "video", "document"}))
 async def send_broadcast(message: Message, state: FSMContext):
-    session = SessionLocal()
-    users = session.query(User).all()
-    print(users)
+    users = get_chat_id()
 
     success = 0
     failed = 0
 
-    print(f"Количество пользователей для рассылки: {len(users)}")
+    await message.answer(f"Количество пользователей для рассылки: {len(users)}")
     for user in users:
-        print(user)
-        print(f"Отправка сообщения пользователю с chat_id: {user.chat_id}")
+        print(f"Отправка сообщения пользователю с chat_id: {user}")
         try:
             # Определяем тип контента
             if message.text:
                 # Рассылка текста
-                await message.bot.send_message(chat_id=str(user.chat_id), text=message.text)
+                await message.bot.send_message(chat_id=str(user), text=message.text)
             elif message.photo:
                 # Рассылка фото
                 await message.bot.send_photo(
-                    chat_id=str(user.chat_id),
+                    chat_id=str(user),
                     photo=message.photo[-1].file_id,
                     caption=message.caption if message.caption else None
                 )
             elif message.video:
                 # Рассылка видео
                 await message.bot.send_video(
-                    chat_id=str(user.chat_id),
+                    chat_id=str(user),
                     video=message.video.file_id,
                     caption=message.caption if message.caption else None
                 )
             elif message.document:
                 # Рассылка документа
                 await message.bot.send_document(
-                    chat_id=str(user.chat_id),
+                    chat_id=str(user),
                     document=message.document.file_id,
                     caption=message.caption if message.caption else None
                 )
@@ -75,7 +51,7 @@ async def send_broadcast(message: Message, state: FSMContext):
 
             success += 1
         except Exception as e:
-            print(f"Не удалось отправить сообщение пользователю {user.chat_id}: {e}")
+            await message.answer(f"Не удалось отправить сообщение пользователю {user}: {e}")
             failed += 1
 
     # Отправляем итог админу
@@ -83,13 +59,12 @@ async def send_broadcast(message: Message, state: FSMContext):
 
     # Завершаем состояние
     await state.clear()
-    session.close()
 
 
 # Обработчик нажатия кнопки "broadcast_message"
 @router.callback_query(F.data == "broadcast_message")
-async def handle_broadcast_button(callback_query: CallbackQuery, state: FSMContext):
+async def handle_broadcast_button(callback: CallbackQuery, state: FSMContext):
     # Переход в состояние ввода текста или медиа для рассылки
-    await callback_query.message.answer("Введите текст или отправьте медиа для рассылки:")
+    await callback.answer()
+    await callback.message.answer("Введите текст или отправьте медиа для рассылки:")
     await state.set_state(NewsLetter.text)
-    await callback_query.answer()
