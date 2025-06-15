dc0c41e30175907225dfe145e6013b5cb612ec5b
DonviteCorleone <blackstar013.bs@gmail.com>
2025-06-15 12:35:02 +0300

    Роутер на подключение администратора подключен в боту.

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -4,7 +4,9 @@ router = Router()
 
 from .handlers.admin_handler import router as admin_handler
 from .handlers.newsletter_handler import router as newsletter_handler
+from .handlers.add_admin_handler import router as add_admin_handler
 
 router.include_router(admin_handler)
 router.include_router(newsletter_handler)
+router.include_router(add_admin_handler)
 
