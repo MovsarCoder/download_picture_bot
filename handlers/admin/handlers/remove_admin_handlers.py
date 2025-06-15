4e40a400a28690fcc8700adb9db31c32f39a73be
DonviteCorleone <blackstar013.bs@gmail.com>
2025-06-15 12:36:15 +0300

    Из модуля admin_handler в отдельный модуль вынесена функция на удаления администратора. Улучшена архитектура

---
+++ handlers/admin/handlers/remove_admin_handlers.py
@@ -0,0 +1,33 @@
+from aiogram import Router, F
+from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery, Message
+
+from keyboard.keyboard import back_keyboard, admin_panel_keyboard
+from keyboard.keyboard_builder import make_row_inline_keyboards
+from States.state import AdminState
+from database.crud import remove_admin
+
+router = Router()
+
+@router.callback_query(F.data == 'remove_admin_list_data')
+async def remove_admin_func(callback: CallbackQuery, state: FSMContext):
+    await callback.answer('')
+    await callback.message.answer('👀Введите ID пользователь, которого хотите удалить', reply_markup=make_row_inline_keyboards(back_keyboard))
+    await state.set_state(AdminState.remove_admin)
+
+
+@router.message(AdminState.remove_admin)
+async def remove_admin_func(message: Message, state: FSMContext):
+    telegram_id = message.text
+    try:
+        # Администратор успешно удален из базы данных.
+        if remove_admin(telegram_id):
+            await message.answer(f'✅Пользователь с ID {telegram_id} был удален!', reply_markup=make_row_inline_keyboards(admin_panel_keyboard))
+
+        else:
+            await message.answer(f'⚠️Предупреждение, не удалось найти администратора для удаления!')
+
+    except ValueError as e:
+        await message.answer(f'❌Ошибка удаления пользователя. Ошибка: {e}', reply_markup=make_row_inline_keyboards(admin_panel_keyboard))
+
+    await state.clear()
