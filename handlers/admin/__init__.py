059409fad4f6cfb85440c5cc40b277760f55a3e7
DonviteCorleone <blackstar013.bs@gmail.com>
2025-06-15 12:54:23 +0300

    Роутер на добавление нового пользователя в вип через амдинку подключен в боту.

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -9,6 +9,7 @@ from .handlers.remove_admin_handlers import router as remove_admin_handler
 from .handlers.add_new_group_handler import router as add_new_group_handler
 from .handlers.delete_group_handler import router as remove_group_handler
 from .handlers.list_group_handler import router as list_group_handler
+from .handlers.add_new_user_vip_handler import router as add_new_vip_user_handler
 
 
 router.include_router(admin_handler)
@@ -18,6 +19,8 @@ router.include_router(remove_admin_handler)
 router.include_router(add_new_group_handler)
 router.include_router(remove_group_handler)
 router.include_router(list_group_handler)
+router.include_router(add_new_vip_user_handler)
+
 
 
 
