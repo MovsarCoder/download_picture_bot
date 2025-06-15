089d85b956a7566c0d46c3243ecce5fd94e204d0
DonviteCorleone <blackstar013.bs@gmail.com>
2025-06-15 12:42:11 +0300

    Роутер на удаление группы подписки подключен в боту.

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -7,6 +7,7 @@ from .handlers.newsletter_handler import router as newsletter_handler
 from .handlers.add_admin_handler import router as add_admin_handler
 from .handlers.remove_admin_handlers import router as remove_admin_handler
 from .handlers.add_new_group_handler import router as add_new_group_handler
+from .handlers.delete_group_handler import router as remove_group_handler
 
 
 router.include_router(admin_handler)
@@ -14,5 +15,6 @@ router.include_router(newsletter_handler)
 router.include_router(add_admin_handler)
 router.include_router(remove_admin_handler)
 router.include_router(add_new_group_handler)
+router.include_router(remove_group_handler)
 
 
