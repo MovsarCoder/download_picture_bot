1b3def9c80d06e30b1f756bc0c64844107759ef7
DonviteCorleone <blackstar013.bs@gmail.com>
2025-06-15 12:39:24 +0300

    Роутер на добавление новой группы подписки подключен в боту.

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -6,10 +6,13 @@ from .handlers.admin_handler import router as admin_handler
 from .handlers.newsletter_handler import router as newsletter_handler
 from .handlers.add_admin_handler import router as add_admin_handler
 from .handlers.remove_admin_handlers import router as remove_admin_handler
+from .handlers.add_new_group_handler import router as add_new_group_handler
+
 
 router.include_router(admin_handler)
 router.include_router(newsletter_handler)
 router.include_router(add_admin_handler)
 router.include_router(remove_admin_handler)
+router.include_router(add_new_group_handler)
 
 
