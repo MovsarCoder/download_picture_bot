3467d9e2ffd0e5da080bb22cc8031bc9521d2771
DonviteCorleone <blackstar013.bs@gmail.com>
2025-07-03 16:44:51 +0300

    Добавлен новый хендлер. В котором можно будет смотреть личную информации про свою вип панель. Сколько дней осталось, когда купил. Какая вип панель и так далее.

--- handlers/vip/handlers/info_vip_panel_handler.py
+++ handlers/vip/handlers/info_vip_panel_handler.py
@@ -5,4 +5,6 @@ router = Router()
 
 @router.callback_query(F.data == 'info_vip_panel')
 async def get_personal_info(callback: CallbackQuery):
+    await callback.answer('')
+
     await callback.message.answer("Ваша личная информация про вип панель!")
