a45938642d9e5c1e6ae118018252d3519428ba47
DonviteCorleone <blackstar013.bs@gmail.com>
2025-07-03 16:18:44 +0300

    Добавлен хендлер на просмотр личной информации пользователя про свою вип панель. Количество дней которое осталось и также когда он купил ее . Его Имя и его айди

--- handlers/vip/__init__.py
+++ handlers/vip/__init__.py
@@ -7,9 +7,11 @@ from .handlers.vip_panel_super_cashback_handler import router as super_cashback_
 from .handlers.vip_panel_ordinary_cashback_handler import router as ordinary_cashback_router
 from .handlers.vip_panel_pars_all_product_handler import router as pars_all_router
 from .handlers.buy_vip_panel import router as buy_vip_router
+from .handlers.info_vip_panel_handler import router as vip_panel_info_router
 
 router.include_router(main_vip_router)
 router.include_router(super_cashback_router)
 router.include_router(ordinary_cashback_router)
 router.include_router(pars_all_router)
 router.include_router(buy_vip_router)
+router.include_router(vip_panel_info_router)
--- handlers/vip/handlers/info_vip_panel_handler.py
+++ handlers/vip/handlers/info_vip_panel_handler.py
@@ -1,5 +1,8 @@
 from aiogram import F, Router
-
+from aiogram.types import CallbackQuery
 
 router = Router()
 
+@router.callback_query(F.data == 'info_vip_panel')
+async def get_personal_info(callback: CallbackQuery):
+    await callback.message.answer("Ваша личная информация про вип панель!")
