44056c3b89da4487e119bfe5f438a58a88e32a1c
DonviteCorleone <blackstar013.bs@gmail.com>
2025-07-24 13:13:17 +0300

    Добавлен запуск шедулера при старте бота.

--- this_bot/run.py
+++ this_bot/run.py
@@ -7,12 +7,18 @@ from config.settings import TOKEN
 from aiogram import Bot, Dispatcher
 from handlers import router
 
+from shedular_start import start_scheduler
+
 
 async def main():
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher()
     dp.include_router(router)
     await bot(DeleteWebhook(drop_pending_updates=True))
+
+    # Запуск Scheduler
+    start_scheduler()
+
     await dp.start_polling(bot, skip_updates=True)
 
 
